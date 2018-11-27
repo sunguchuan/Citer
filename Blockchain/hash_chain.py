7ccdd1966f176d13ad4a0c9390ecafd89535f846
sunguchuan <31227684+sunguchuan@users.noreply.github.com>
2018-11-27 16:01:20 -0500

    Update hash_chain.py

--- Blockchain/hash_chain.py
+++ Blockchain/hash_chain.py
@@ -1,7 +1,6 @@
 import sys, os
 import _thread
 import time
-import subprocess
 
 import requests
 from flask import Flask, jsonify, request
@@ -23,13 +22,3 @@ if __name__ == '__main__':
         j=5000+i
         node=f'localhost:{j}'
         print(f'Hashed blockchain from {node}:',get_hash(node))
-# time.sleep(5)
-# os.system('''Curl -X POST -H "Content-Type:application/json" -d '{"nodes":["http://localhost:5001/"]}' "http://localhost:5000/nodes/register"''')
-#
-# os.system('''Curl -X POST -H "Content-Type:application/json" -d '{"nodes":["http://localhost:5001/"]}' "http://localhost:5002/nodes/register"''')
-#
-# os.system('''Curl -X POST -H "Content-Type:application/json" -d '{"nodes":["http://localhost:5002/"]}' "http://localhost:5000/nodes/register"''')
-#
-# os.system('''curl -X POST -H "Content-Type:application/json" -d '{"pubkey":"/Users/user/Desktop/sgc/Project/Data/code/test/public.pub", "info":"/Users/user/Desktop/sgc/Project/Data/code/test/info.txt", "govsig": "/Users/user/Desktop/sgc/Project/Data/code/test/fromgov.bin"}' "http://localhost:5000/data/new"''')
-
-#,stdin=subprocess.PIPE, stderr=subprocess.PIPE,stdout=subprocess.PIPE
